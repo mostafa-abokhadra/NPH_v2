3b5139a49b5cd8079b23857d55c867b69218fb09
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-16 03:22:31 +0300

    some

--- NursePatientHub/app.py
+++ NursePatientHub/app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__, static_url_path='/static')
 
-
 @app.route('/', strict_slashes=False)
 @app.route('/home', strict_slashes=False)
 @app.route('/NPH', strict_slashes=False)
@@ -14,14 +13,8 @@ def home():
 
 @app.route('/signUp', methods=["POST", "GET"])
 def signUp():
-    if request.method == 'POST':
-        return redirect(url_for("test", request.POST))
     title = signUp
     return render_template('signUp.html', title=title)
-
-@app.route('/test/<req>')
-def test(req):
-    return render_template('test.html', req=req)
 
 if __name__ == '__main__':
     app.run(debug=True)
