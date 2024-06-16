48a528575e2b2469024a6713e3d1e6457a58d76d
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-16 22:32:14 +0300

    __init__

--- NursePatientHub/launch_db.py
+++ NursePatientHub/launch_db.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-engine = __import__('models/database.create_engine').engine
+from models.__init__ import engine
 from models.base import Base
 from models.patient import Patient
 from models.nurse import Nurse
