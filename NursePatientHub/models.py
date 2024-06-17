e7a096febdce7d91d67e8821b35f3b67db1c78db
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-17 03:29:41 +0300

    working on many to many

--- NursePatientHub/models.py
+++ NursePatientHub/models.py
@@ -6,24 +6,48 @@ from sqlalchemy.orm import sessionmaker, relationship, backref
 engine = create_engine(f"mysql+mysqldb://{'database_admin'}:{'NPH_db_admin'}@localhost/{'NPH'}", pool_pre_ping=True)
 base = declarative_base()
 
-class Base:
+class User(base):
+    __tablename__ = 'Users'
+    id = Column(Integer, nullable=False, primary_key=True)
     firstName = Column(String(50), nullable=False)
     lastName = Column(String(50), nullable=False)
-
-class Nurse(Base, base):
+    email = Column(String(100), nullable=False)
+    password = Column(String(20), nullable=False)
+   
+class Nurse(User, base):
     __tablename__ = "Nurses"
     id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("Users.id", ondelete="CASCADE", onupdate="CASCADE"))
     specialist = Column(String(50))
     degree = Column(String(50))
     employemnt_status = Column(String(50))
-    homeCarePatient = relationship("Patient", back_populates="nurse", cascade="all, delete, save-update")
 
-class Patient(Base, base):
+class Patient(account, base):
     __tablename__ = 'Patients'
     id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
-    third_name = Column(String(50), nullable=False)
-    fourth_name = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey("Users.id", ondelete="CASCADE", onupdate="CASCADE"))
     nurse_id = Column(Integer, ForeignKey("Nurses.id", ondelete="CASCADE", onupdate="CASCADE"))
-    nurse = relationship("Nurse", back_populates="homeCarePatient")
+    diagnosis = Column(String(50))
+
+class Employer(User, base):
+    __tablename__ = "Employers"
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("Users.id", ondelete="CASCADE", onupdate="CASCADE"))
+    organization_name = Column(String(50))
+
+class Application(base):
+    __tablename__ = 'Applications'
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("Employers.id", ondelete="CASCADE", onupdate="CASCADE"))
+    country = Column(String(50))
+    city = Column(String(50))
+    organization_name = Column(String(50))
+    organization_address = Column(String(200))
+    referred_by = Column(String(50))
+    position = Colum(String(100))
+    education_requirements(String(200))
+    special_skills = Column(String(200))
+    experience_years = Column(String(50))
+
 
 base.metadata.create_all(bind=engine)
