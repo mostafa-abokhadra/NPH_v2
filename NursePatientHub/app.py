c6af0cef87e87e27f7df9e80371f840f7d8f6519
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-18 01:00:23 +0300

    sign up login

--- NursePatientHub/app.py
+++ NursePatientHub/app.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 """starting point"""
 from flask import Flask, render_template, url_for, request, redirect
+from flask_restful import Api, Resource, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://database_admin:NPH_db_admin@localhost/NPH'
+db = SQLAlchemy(app)
 
 @app.route('/', strict_slashes=False)
 @app.route('/home', strict_slashes=False)
 @app.route('/NPH', strict_slashes=False)
 def home():
-    title = 'NPH'
-    return render_template('home.html', title=title)
+    return render_template('home.html')
 
 @app.route('/signUp', methods=["POST", "GET"])
 def signUp():
-    title = signUp
-    return render_template('signUp.html', title=title)
+    if request.method == 'POST':
+        newuser = request.form
+        return redirect(url_for('homewoutsignup'))
+    return render_template('signUp.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
