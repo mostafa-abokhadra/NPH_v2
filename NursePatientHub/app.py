12868fd21c52557c213a42cbb675044a12df9f5d
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-18 03:48:18 +0300

    added userType html css

--- NursePatientHub/app.py
+++ NursePatientHub/app.py
@@ -69,26 +69,50 @@ def home():
 def login():
     return render_template("login.html")
 
+def check_name_validity(req):
+    if len(req["firstName"]) + len(req["lastName"]) <= 10 or len(req["firstName"]) + len(req["lastName"]) >= 30:
+        return 1
+    elif any(not letter.isalnum() for letter in req["firstName"]):
+        return 2
+    elif any(not letter.isalnum() for letter in req["lastName"]):
+        return 2
 
-def check_email_validity(req):
-    check_email_exists = User.query.filter(User.email == req["email"]).first()
+def check_email_validity(email):
+    check_email_exists = User.query.filter(User.email == email).first()
     if (check_email_exists == None):
         return 1
 
 def check_pass_validity(req):
     if len(req["password"]) < 8:
         return 1
+    elif not req["password"] == req["confirm-pass"]:
+        return 2
+
+@app.route('/userType', strict_slashes=False)
+def userType():
+    return render_template('userType.html')
 
 @app.route('/signUp', methods=["POST", "GET"])
 def signUp():
     if request.method == 'POST':
-        if not check_email_validity(request.form):
+        if check_name_validity(request.form) == 1:
+            flash("full name can't be less than 10 or greater than 20 character !")
+            return redirect(url_for('signUp'))
+        elif check_name_validity(request.form) == 2:
+            flash("name can't contain special character")
+            return redirect(url_for('signUp'))
+        elif not check_email_validity(request.form["email"]):
             flash("email already exists!")
             return redirect(url_for('login'))
-        elif check_pass_validity(request.form):
+        elif check_pass_validity(request.form) == 1:
             flash("password can't be less than 8 characters")
             return redirect(url_for('signUp'))
-        
+        elif check_pass_validity(request.form) == 2:
+            flash("confirm the pass correctly !")
+            return redirect(url_for('signUp'))
+        else:
+            flash("{} added successfully !".format(request.form["firstName"]))
+            return redirect(url_for('userType'))
     return render_template('signUp.html')
 
 if __name__ == '__main__':
