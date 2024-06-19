8e0bd129d2ff0256cb46d8c207eff4d83b23f3ac
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-19 04:16:18 +0300

    login css

--- NursePatientHub/static/js/signUp.js
+++ NursePatientHub/static/js/signUp.js
@@ -1,14 +1,29 @@
-mydata = {}
-document.forms[0].onclick = function (event) {
-    event.preventDefault();
-    let fname = document.querySelector("[name='firstName']")
-    let lname = document.querySelector("[name='lastName']")
-    let email = document.querySelector("[name='email']")
-    let pass = document.querySelector(".pass")
-    let mydata = {
-        "firstName": fname.value,
-        "lastName": lname.value,
-        "email": email.value,
-        "password": pass.value
+// document.forms[0].onclick = function (event) {
+//     // event.preventDefault();
+//     mydata = {}
+//     let fname = document.querySelector("[name='firstName']")
+//     let lname = document.querySelector("[name='lastName']")
+//     let email = document.querySelector("[name='email']")
+//     let pass = document.querySelector(".pass")
+//     let mydata = {
+//         "firstName": fname.value,
+//         "lastName": lname.value,
+//         "email": email.value,
+//         "password": pass.value
+//     }
+// }
+let fname = document.querySelector("[name='firstName']")
+for (let i = 0; i < fname.value.length; i++) {
+    if (fname.value[i] >= 'a' && fname.value[i] <= 'z') {
+        continue;
+    } else {
+        let inp = document.querySelector('.firstName');
+        console.log(inp);
     }
 }
+let lname = document.querySelector("[name='lastName']")
+let email = document.querySelector("[name='email']")
+let pass = document.querySelector(".pass")
+
+
+
