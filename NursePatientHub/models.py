83a2f6b85f8c4f5d0fbdc680d5df8e0805c94eba
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-23 08:54:56 +0300

    good night

--- NursePatientHub/models.py
+++ NursePatientHub/models.py
@@ -1,52 +1,52 @@
-from NursePatientHub import db, app
-from flask_login import UserMixin
+# from NursePatientHub import db, app
+# from flask_login import UserMixin
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'Users'
-    User_id = db.Column(db.Integer, primary_key=True,unique=True, autoincrement=True)
-    username = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(60), nullable=False)
-    userType = db.Column(db.CHAR(1), nullable=False)
+# class User(db.Model, UserMixin):
+#     __tablename__ = 'Users'
+#     User_id = db.Column(db.Integer, primary_key=True,unique=True, autoincrement=True)
+#     username = db.Column(db.String(20), nullable=False)
+#     email = db.Column(db.String(50), nullable=False, unique=True)
+#     password = db.Column(db.String(60), nullable=False)
+#     userType = db.Column(db.CHAR(1), nullable=False)
    
-class Nurse(User, db.Model):
-    __tablename__ = "Nurses"
-    Nurse_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("Users.User_id", ondelete="CASCADE", onupdate="CASCADE"))
-    specialist = db.Column(db.String(50))
-    degree = db.Column(db.String(50))
-    employemnt_status = db.Column(db.String(50))
+# class Nurse(User, db.Model):
+#     __tablename__ = "Nurses"
+#     Nurse_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey("Users.User_id", ondelete="CASCADE", onupdate="CASCADE"))
+#     specialist = db.Column(db.String(50))
+#     degree = db.Column(db.String(50))
+#     employemnt_status = db.Column(db.String(50))
 
-class Patient(User, db.Model):
-    __tablename__ = 'Patients'
-    Patient_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("Users.User_id", ondelete="CASCADE", onupdate="CASCADE"))
-    nurse_id = db.Column(db.Integer, db.ForeignKey("Nurses.Nurse_id", ondelete="CASCADE", onupdate="CASCADE"))
-    diagnosis = db.Column(db.String(50))
+# class Patient(User, db.Model):
+#     __tablename__ = 'Patients'
+#     Patient_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey("Users.User_id", ondelete="CASCADE", onupdate="CASCADE"))
+#     nurse_id = db.Column(db.Integer, db.ForeignKey("Nurses.Nurse_id", ondelete="CASCADE", onupdate="CASCADE"))
+#     diagnosis = db.Column(db.String(50))
 
-class Employer(User, db.Model):
-    __tablename__ = "Employers"
-    Employer_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("Users.User_id", ondelete="CASCADE", onupdate="CASCADE"))
-    application = db.relationship("Application", back_populates="employer", cascade="all, delete, save-update")
-    organization_name = db.Column(db.String(50))
+# class Employer(User, db.Model):
+#     __tablename__ = "Employers"
+#     Employer_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey("Users.User_id", ondelete="CASCADE", onupdate="CASCADE"))
+#     application = db.relationship("Application", back_populates="employer", cascade="all, delete, save-update")
+#     organization_name = db.Column(db.String(50))
 
-class Application(db.Model):
-    __tablename__ = 'Applications'
-    id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("Employers.Employer_id", ondelete="CASCADE", onupdate="CASCADE"))
-    employer = db.relationship("Employer", back_populates="application")
-    country = db.Column(db.String(50))
-    city = db.Column(db.String(50))
-    organization_name = db.Column(db.String(50))
-    organization_address = db.Column(db.String(200))
-    referred_by = db.Column(db.String(50))
-    position = db.Column(db.String(100))
-    education_requirements = db.Column(db.String(200))
-    # special_skills = db.Column(db.String(200))
-    experience_years = db.Column(db.String(50))
-    salary = db.Column(db.Integer)
-    currency = db.Column(db.String(60))
+# class Application(db.Model):
+#     __tablename__ = 'Applications'
+#     id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey("Employers.Employer_id", ondelete="CASCADE", onupdate="CASCADE"))
+#     employer = db.relationship("Employer", back_populates="application")
+#     country = db.Column(db.String(50))
+#     city = db.Column(db.String(50))
+#     organization_name = db.Column(db.String(50))
+#     organization_address = db.Column(db.String(200))
+#     referred_by = db.Column(db.String(50))
+#     position = db.Column(db.String(100))
+#     education_requirements = db.Column(db.String(200))
+#     # special_skills = db.Column(db.String(200))
+#     experience_years = db.Column(db.String(50))
+#     salary = db.Column(db.Integer)
+#     currency = db.Column(db.String(60))
 
-# with app.app_context():
-#     db.create_all()
+# # with app.app_context():
+# #     db.create_all()
--- NursePatientHub/routes.py
+++ NursePatientHub/routes.py
@@ -1,12 +1,62 @@
-from NursePatientHub import app, bcrypt
+from NursePatientHub import app, bcrypt, db
 from flask import render_template, url_for, request, redirect, flash
-from NursePatientHub.models import User, Patient, Nurse, Employer, Application
+# from NursePatientHub.models import User, Patient, Nurse, Employer, Application
 from NursePatientHub.forms import Registration, Login
-from flask_login import login_user, LoginManager, login_required, logout_user, current_user
+from flask_login import login_user, UserMixin, LoginManager, login_required, logout_user, current_user
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'Users'
+    User_id = db.Column(db.Integer, primary_key=True,unique=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+    userType = db.Column(db.CHAR(1), nullable=False)
+   
+class Nurse(db.Model):
+    __tablename__ = "Nurses"
+    Nurse_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.User_id", ondelete="CASCADE", onupdate="CASCADE"))
+    specialist = db.Column(db.String(50))
+    degree = db.Column(db.String(50))
+    employemnt_status = db.Column(db.String(50))
+
+class Patient(db.Model):
+    __tablename__ = 'Patients'
+    Patient_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.User_id", ondelete="CASCADE", onupdate="CASCADE"))
+    nurse_id = db.Column(db.Integer, db.ForeignKey("Nurses.Nurse_id", ondelete="CASCADE", onupdate="CASCADE"))
+    diagnosis = db.Column(db.String(50))
+
+class Employer(db.Model):
+    __tablename__ = "Employers"
+    Employer_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.User_id", ondelete="CASCADE", onupdate="CASCADE"))
+    application = db.relationship("Application", back_populates="employer", cascade="all, delete, save-update")
+    organization_name = db.Column(db.String(50))
+
+class Application(db.Model):
+    __tablename__ = 'Applications'
+    id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("Employers.Employer_id", ondelete="CASCADE", onupdate="CASCADE"))
+    employer = db.relationship("Employer", back_populates="application")
+    country = db.Column(db.String(50))
+    city = db.Column(db.String(50))
+    organization_name = db.Column(db.String(50))
+    organization_address = db.Column(db.String(200))
+    referred_by = db.Column(db.String(50))
+    position = db.Column(db.String(100))
+    education_requirements = db.Column(db.String(200))
+    # special_skills = db.Column(db.String(200))
+    experience_years = db.Column(db.String(50))
+    salary = db.Column(db.Integer)
+    currency = db.Column(db.String(60))
+
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -31,28 +81,26 @@ def signUp():
         if email:
             flash("email already exists! try to login")
             return redirect(url_for('login'))
-        from NursePatientHub import db
+        # from NursePatientHub import db
         hasshed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User(password=hasshed_password, username=form.username.data,
             email=form.email.data, userType=form.userType.data)
-        # new_user = User(username="mostafa", email="mostaf@gmail.com", userType='N', password="messimessi")
+        db.session.add(new_user)
+        db.session.commit()
         if form.userType.data == 'N':
-            nurse = Nurse()
-            nurse.user_id = new_user.User_id
-            db.session.add(new_user)
-            db.session.add(nurse)
+            n = Nurse()
+            n.user_id = new_user.User_id
+            db.session.add(n)
+            db.session.commit()
         elif form.userType.data == 'P':
             patient = Patient()
             patient.user_id = new_user.User_id
-            db.session.add(new_user)
             db.session.add(patient)
+            db.session.commit()
         else:
             emp = Employer()
             emp.user_id = new_user.User_id
-            db.session.add(new_user)
             db.session.add(emp)
-        with app.app_context():
-            print("asldfjklasjd;ljsdfa;sldj;sjdalfk;sldajf")
             db.session.commit()
         return render_template('dashBoard.html')
     return render_template('signUp.html', form=form)
@@ -63,10 +111,12 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter(User.email == form.email.data).first()
         if user:
+            print("hello")
             if bcrypt.check_password_hash(user.password, form.password.data):
+                print("hi")
                 login_user(user)
                 return redirect(url_for('dashBoard'))
-    return render_template("login.html")
+    return render_template("login.html", form=form)
 
 @app.route('/logout', methods=['POST', 'GET'])
 @login_required
