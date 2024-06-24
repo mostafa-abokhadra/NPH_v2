1fdd7335e2ff044a7f493716f2e0405719283376
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-25 01:12:09 +0300

    application backend

--- NursePatientHub/static/js/applications.js
+++ NursePatientHub/static/js/applications.js
@@ -3,8 +3,16 @@ logo_div.onclick = function () {
     let url = "http://127.0.0.1:5000/";
     location.href = url;
 }
-let countries = document.querySelector('.position')
-countries.onclick = function () {
+
+let countries_list = document.querySelector('#countries-list')
+let countries = document.querySelector('#countries')
+countries.addEventListener('focus', adding_countries)
+
+let cities = document.querySelector('#cities')
+let cities_list = document.querySelector('#cities-list')
+cities.addEventListener('focus', adding_cities)
+
+function adding_countries() {
     fetch("https://countriesnow.space/api/v0.1/countries",
         // {method: "POST",
         // headers: { 'Content-Type': 'application/json' },
@@ -12,15 +20,19 @@ countries.onclick = function () {
     )
         .then((res) => {
             if (!res.ok) {
-                console.log('cant fetch an api')
-                throw new Error("can't fetch an api")
+                console.log('cant fetch an api for countries')
+                throw new Error("can't fetch countries api")
             }
             return res.json()
         }).then((data) => {
             let arr = Array.from(data["data"])
             // console.log(arr)
             for (let i = 0; i < arr.length; i++) {
-                console.log(arr[i]["country"])
+                let opt = document.createElement("option")
+                opt.setAttribute('value', arr[i]["country"])
+                opt.textContent = arr[i]["country"]
+                countries_list.appendChild(opt)
+                // console.log(opt)
                 // console.log(arr[0])
                 // console.log(arr[i]["data"]["country"] + "::: " + arr[i]]["data"]["country"]["cities"])
             }
@@ -28,18 +40,55 @@ countries.onclick = function () {
         .catch((error) => {
             console.error("fetch problem")
         });
+    countries.removeEventListener('focus', adding_countries)
 }
-let hospitals = document.querySelector('.org-name')
-hospitals.onclick = function () {
-    fetch('http://www.communitybenefitinsight.org/api/get_hospital_data.php?hospital_id=1000', { mode: 'no-cors' })
-        .then(async (res) => {
-            if (!res.ok) {
-                console.log("failed to fetch hospitals");
-                return;
-            }
-            let data = await res.text()
-            console.log(data)
-        }).catch((err) => {
-            console.error("error fetching hospitals: ", err)
-        })
-}
+console.log('messi');
+console.log(countries.value);
+// let countries_opt = querySelector()
+
+// function adding_cities() {
+//     fetch("https://countriesnow.space/api/v0.1/countries",
+// {method: "POST",
+// headers: { 'Content-Type': 'application/json' },
+// body: JSON.stringify({ "country": "egypt" })}
+// )
+//     .then((res) => {
+//         if (!res.ok) {
+//             console.log('cant fetch an api for cities')
+//             throw new Error("can't fetch cities api")
+//         }
+//         return res.json()
+//     }).then((data) => {
+//         let arr = Array.from(data["data"])
+//         // console.log(arr)
+//         for (let i = 0; i < arr.length; i++) {
+//             console.log(arr[i])
+// let opt = document.createElement("option")
+// opt.setAttribute('value', arr[i]["country"])
+// opt.textContent = arr[i]["country"]
+// countries_list.appendChild(opt)
+// console.log(opt)
+// console.log(arr[0])
+// console.log(arr[i]["data"]["country"] + "::: " + arr[i]]["data"]["country"]["cities"])
+//             }
+//         })
+//         .catch((error) => {
+//             console.error("fetch problem")
+//         });
+//     cities.removeEventListener('focus', adding_cities)
+// }
+
+// let hospitals = document.querySelector('.org-name')
+// hospitals.onclick = function () {
+//     fetch('http://www.communitybenefitinsight.org/api/get_hospital_data.php?hospital_id=1000', { mode: 'no-cors' })
+//         .then(async (res) => {
+//             if (!res.ok) {
+//                 console.log("failed to fetch hospitals");
+//                 return;
+//             }
+//             let data = await res.text()
+//             console.log(data)
+//         }).catch((err) => {
+//             console.error("error fetching hospitals: ", err)
+//         })
+// }
