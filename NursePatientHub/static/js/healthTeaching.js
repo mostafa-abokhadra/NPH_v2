f0186140fb2089b5e860e375409dbf6fa46efabf
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-28 15:55:05 +0300

    updating front side

--- NursePatientHub/static/js/healthTeaching.js
+++ NursePatientHub/static/js/healthTeaching.js
@@ -15,28 +15,13 @@
 //     console.log(err)
 // }
 
-try {
-    let plus_logo = document.querySelector('.ask .plus-logo')
-    plus_logo.addEventListener('click', popQuestion)
-    let ques = document.querySelector('.q-form')
-    async function popQuestion() {
-        ques.setAttribute('style', 'display: block;')
-    }
-    let myBtn = document.querySelector('.q-submit')
-    myBtn.addEventListener('click', sendQuestion)
-    async function sendQuestion() {
-        ques.setAttribute('style', 'display: none;')
-
-    }
-} catch (err) {
-    console.log("can't popUp question input")
-}
-
+/*if user is not authenticated then this code run if he tries to access populate healtTeaching
+or ask a question properities, he must singUp first*/
 let elem = document.createElement('small')
 elem.textContent = "login to access this service"
 elem.setAttribute('style', 'color: red; align-self: center; padding-left: 5px;')
 let parent = document.querySelectorAll('.styling-div')
-/**/
+
 try {
     let plus_not_populate = document.querySelector('.populate .plus-logo-not')
     let plus_not_ask = document.querySelector('.ask .plus-logo-not')
@@ -53,29 +38,22 @@ try {
 } catch (err) {
     console.log("some thing went wrong with error message of 'you need to login first'")
 }
-/**/
+/*end*/
+
+/*if current user is nurse or employer and tries to ask question as patients this code run*/
 try {
-    let notNurse = document.querySelector('.plus-logo-notNurse')
-    notNurse.addEventListener('click', invlide_user_nurse)
-    async function invlide_user_nurse() {
-        elem.textContent = "only Nurses have access to this page"
-        parent[0].appendChild(elem)
+    let notPatient = document.querySelector('.plus-logo-not-patient')
+    notPatient.addEventListener('click', invlide_user_patient)
+    async function invlide_user_patient() {
+        elem.textContent = "only patinets have access to this page"
+        parent[1].appendChild(elem)
     }
 } catch (err) {
     console.log(err)
 }
-/**/
-// try {
-//     let notPatient = document.querySelector('.plus-logo-not-patient')
-//     notPatient.addEventListener('click', invlide_user_patient)
-//     async function invlide_user_patient() {
-//         elem.textContent = "only patinets have access to this page"
-//         parent[1].appendChild(elem)
-//     }
-// } catch (err) {
-//     console.log(err)
-// }
-/**/
+/*end*/
+
+/* whan a nurse click on answer link to a patient questoin, small window will popUp */
 let answer_link = document.querySelector('.answer-link')
 try {
     let goTo = document.querySelector('.go-to-popup')
@@ -92,14 +70,55 @@ try {
 } catch (err) {
     console.log(err)
 }
-/**/
+/*end*/
+
+/* if a patient or employer tries to populate healthTeaching this code runs*/
 try {
-    let no_popup = document.querySelector('.no-popup')
-    no_popup.addEventListener('click', noPopUp)
-    async function noPopUp() {
-        elem.textContent = 'only Nurses can answer patients questions'
-        answer_link.insertBefore(elem, answer_link.firstChild)
+    let notNurse = document.querySelector('.plus-logo-notNurse')
+    notNurse.addEventListener('click', invlide_user_nurse)
+    async function invlide_user_nurse() {
+        elem.textContent = "only Nurses have access to this page"
+        parent[0].appendChild(elem)
     }
 } catch (err) {
     console.log(err)
 }
+/* end */
+
+/* if a patient or employer tries to answer patient question this code runs */
+try {
+    let no_popup = document.querySelector('.no-popup')
+    let place = document.querySelector('#answer_me')
+    let parent = document.querySelector('#questionsToAnswer')
+
+    let new_elem = document.createElement('div')
+    new_elem.setAttribute('class', 'only-nurses')
+
+    no_popup.addEventListener('click', noPopUp)
+    async function noPopUp() {
+        new_elem.textContent = 'only Nurses can answer patients questions'
+        parent.insertBefore(new_elem, place)
+    }
+} catch (err) {
+    console.log(err)
+}
+/* end */
+
+/* when patient click on ask question small input window shows */
+try {
+    let plus_logo = document.querySelector('.ask .plus-logo')
+    plus_logo.addEventListener('click', popQuestion)
+    let ques = document.querySelector('.q-form')
+    async function popQuestion() {
+        ques.setAttribute('style', 'display: block;')
+    }
+    let myBtn = document.querySelector('.q-submit')
+    myBtn.addEventListener('click', sendQuestion)
+    async function sendQuestion() {
+        ques.setAttribute('style', 'display: none;')
+
+    }
+} catch (err) {
+    console.log(err)
+}
+/* end */
