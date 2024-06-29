70c662f5f5c104128668d4f26aae88d602d1f08d
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-29 13:00:31 +0300

    updating database to aiven

--- NursePatientHub/__init__.py
+++ NursePatientHub/__init__.py
@@ -8,8 +8,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = os.urandom(12)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://database_admin:NPH_db_admin@localhost/NPH'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://avnadmin:AVNS_dkQO0y4LKX_S1PM1VvO@nph-mbukhadra-nph.f.aivencloud.com:22413/NPH'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://database_admin:NPH_db_admin@localhost/NPH'
 app.config.from_object(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
